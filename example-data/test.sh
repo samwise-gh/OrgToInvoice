d20185fdea5d54f086624fbb5030556a53850dd6
samwise-gh <samwise_i@yahoo.com>
2022-01-07 13:18:53 -0800

    Updated README.md and examples, added Python script

--- example-data/test.sh
+++ example-data/test.sh
@@ -2,5 +2,5 @@
 
 # Run a report for pay Feb 2017 pay period 1 (1-14)
 
-../bin/orgtoinv timeproc --PO 314159 --invoice 42 --month 02 --period 1 --year 2017 --time  ./customer.org -v
+orgtoinv --PO 314159 --invoice 42 --month 02 --period 1 --year 2017 --time ./customer.org -v
 
